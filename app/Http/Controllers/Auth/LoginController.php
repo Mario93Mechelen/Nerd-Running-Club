539e5d4bebc019efd740a1b7d579a14d1aca2224
Mario93Mechelen <r0334050@student.thomasmore.be>
2017-10-04 16:17:32 +0200

    login works backend

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,27 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function redirectToProvider()
+    {
+        return redirect('https://www.strava.com/oauth/authorize?client_id=20594&response_type=code&redirect_uri=http://homestead.app/oauth/code_callback&scope=write&state=mystate&approval_prompt=force');
+    }
+
+    public function handleProviderCallback()
+    {
+        $code = request()->code;
+        $client = new \GuzzleHttp\Client();
+        //$url = "'https://www.strava.com/oauth/token?client_id=20594&client_secret=426f99ae57f2c243fdcc6e5fa320c011523c6161&code=".$code."'";
+        $res = $client->request('POST', 'https://www.strava.com/oauth/token', [
+            'form_params' => [
+                'client_id' => '20594',
+                'client_secret' => '426f99ae57f2c243fdcc6e5fa320c011523c6161',
+                'code' => $code,
+            ]
+        ]);
+
+        $result= json_decode($res->getBody());
+        var_dump($result);
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login/strava', 'Auth\LoginController@redirectToProvider');
+Route::get('oauth/code_callback', 'Auth\LoginController@handleProviderCallback');
