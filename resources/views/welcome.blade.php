a27d7be6a812f9c569fb788bc60d51b145ae71ab
Helena Sollie <helena.sollie@gmail.com>
2017-10-10 22:36:23 +0200

    Added layout 2.0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,91 +5,82 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Nerd Running Club</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
 
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
+
+            body {
+
+
+                width: 80%;
+                margin-left: 10%;
+                font-family: 'Lato', sans-serif;
+                font-weight: 300;
             }
 
-            .full-height {
-                height: 100vh;
-            }
+            .loginStrava {
 
-            .flex-center {
-                align-items: center;
+                background-color: #8CBD9C;
+                width: 100%;
+                height: 100vh;
                 display: flex;
+                flex-direction: column;
+            }
+
+            .loginStrava img {
+
+                width:200px;
+                height: auto;
+                margin-top: 200px;
+                align-self: center;
+            }
+
+            .loginStrava a {
+
+                background-color: white;
+                width: 300px;
+                align-self: center;
+                padding: 2%;
+                display: flex;
+                flex-direction: row;
                 justify-content: center;
+
+                border-radius: 10px;
+                margin-top: 50px;
             }
 
-            .position-ref {
-                position: relative;
-            }
+            .loginStrava a p {
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
                 text-decoration: none;
+                color: #90C8C8;
+                font-size: 1em;
                 text-transform: uppercase;
             }
 
-            .m-b-md {
-                margin-bottom: 30px;
+            .loginStrava a img {
+
+                width: 100px;
+                height: 20px;
+                margin-top: 2px;
+                margin-left: 15px;
             }
+
+
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+        <div class="loginStrava">
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+            <img src="{{URL::asset('/img/logorunapp.png')}}" alt="Logo Nerd Running App">
+
+            <a><p>Login with</p><img src="{{URL::asset('/img/stravalogo.png')}}" alt="profile Pic" height="50" width="200"></a>
+
+
         </div>
     </body>
 </html>
