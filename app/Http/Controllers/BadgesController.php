1112ecccd8324add39922c1bfe44a97041607e2e
Helena Sollie <helena.sollie@gmail.com>
2017-11-07 15:56:13 +0100

    function for badges

--- app/Http/Controllers/BadgesController.php
+++ app/Http/Controllers/BadgesController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Badges;
+use App\Userbadges;
+use Illuminate\Support\Facades\Auth;
 
 class BadgesController extends Controller
 {
@@ -14,6 +16,18 @@ class BadgesController extends Controller
 
         $badge = Badges::all();
         return view('layouts.achievements', compact('badge'));
+
+
+    }
+
+    public function achievedBadges()
+    {
+
+        $id = Auth::user()->id;
+
+        $achieved =  Userbadges::all()->where('user_id', $id);
+
+        return view('layouts.achievements', compact('achieved'));
 
 
     }
