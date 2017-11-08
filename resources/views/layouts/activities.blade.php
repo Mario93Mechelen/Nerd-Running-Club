eb09079e725c4a0996b82f83e13d3b68b3e4eac3
Helena Sollie <helena.sollie@gmail.com>
2017-11-08 09:16:51 +0100

    Pushed time in activities

--- resources/views/layouts/activities.blade.php
+++ resources/views/layouts/activities.blade.php
@@ -1,52 +1,6 @@
 @extends('master')
 
 @section('content')
-    <style>
-
-    .latestruns {
-
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        width: 80%;
-        margin-left: 10%;
-        margin-bottom: 100px;
-    }
-
-
-    .run {
-
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-start;
-            margin-top: 20px;
-            margin-bottom: 20px;
-            border: 1px solid #FCF0BB;
-            padding: 20px;
-        }
-
-    .run #date h1 {
-
-        font-size: 1em;
-        color: #1b6d85;
-        text-align: center;
-    }
-
-    .run #info {
-
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        width: 80%;
-        margin-left: 10%;
-    }
-
-    .run #info h3{
-        color: #2ab27b;
-
-    }
-
-    </style>
 
     <div class="latestruns">
 
@@ -65,16 +19,25 @@
                 <div id="distance">
 
                     <h3>Distance</h3>
-                    <p>{{$a->distance}} km</p>
+                    <p>{{$a->distance}} m</p>
 
                 </div>
 
                 <div id="time">
 
-                    <h3>Time</h3>
-                    <p>{{$a->averageSpeed}} minutes</p>
+                    <h3>Av. Speed</h3>
+                    <p>{{$a->averageSpeed}} m/s</p>
 
                 </div>
+
+                <div id="time">
+
+                    <h3>Total time</h3>
+                    <p>{{$a->time}} sec</p>
+
+                </div>
+
+
 
             </div>
 
