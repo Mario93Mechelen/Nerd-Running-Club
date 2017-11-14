29d730bdbfc8382720a35a68120c3e6bc6501444
Mario93Mechelen <r0334050@student.thomasmore.be>
2017-11-14 12:53:01 +0100

    changed procedures for login and scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('db:update')->everyMinute();
+        $schedule->command('db:update')->hourly(30);
     }
 
     /**
