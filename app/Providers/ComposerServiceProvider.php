8b58f3a697fd5326ad85c601a45e8c9ba88fa854
Mario93Mechelen <r0334050@student.thomasmore.be>
2017-11-17 13:13:34 +0100

    small change

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -14,7 +14,7 @@ class ComposerServiceProvider extends ServiceProvider
      */
     public function boot(ViewFactory $view)
     {
-        $view->composer('*', 'App\Http\ViewComposers\GlobalComposer');
+        $view->composer('*', 'App\Http\Viewcomposers\GlobalComposer');
     }
 
     /**
